d658d3e5a9fc52e7832d5140db261bcf142b8670
Jiachang Liu <jiachang.liu@duke.edu>
2025-03-22 23:38:34 +0000

    fix: help github workflow build and therefore find armadillo

--- python/scripts/install_linux_libs.sh
+++ python/scripts/install_linux_libs.sh
@@ -3,8 +3,18 @@
 # Based on https://github.com/mlpack/mlpack-wheels
 # Based on https://gitlab.com/jason-rumengan/pyarma
 
+# Install OpenBLAS
 basedir=$(python3 scripts/openblas_support.py)
 $use_sudo cp -r $basedir/lib/* /usr/local/lib
 $use_sudo cp $basedir/include/* /usr/local/include
 
+# Build and install Armadillo from the submodule
+cd external/armadillo-code
+cmake -DCMAKE_INSTALL_PREFIX=/usr/local .
+make -j4
+$use_sudo make install
+cd ../..
+
+# Set library paths
 export DYLD_FALLBACK_LIBRARY_PATH=/usr/local/lib/
+export LD_LIBRARY_PATH=/usr/local/lib/:$LD_LIBRARY_PATH
